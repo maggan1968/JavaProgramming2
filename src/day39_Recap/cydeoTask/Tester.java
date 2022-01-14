396f290785ce79173479af70b18df91c520dbcbc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-14 16:02:17 -0500

    day39: Recap

--- src/day39_Recap/cydeoTask/Tester.java
+++ src/day39_Recap/cydeoTask/Tester.java
@@ -10,6 +10,12 @@ public class Tester extends Employee{
         System.out.println(getJobTitle() +" "+getName()+" is creating ticket");
     }
 
+    @Override
+    public void work() {
+        System.out.println(getJobTitle()+" "+getName()+" is testing the application");
+    }
+
+
 }
 /*
 3. Create a sub class of Employee named Tester
