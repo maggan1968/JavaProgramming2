9d4cc8d04354cec93e7d09df3e415c925e4fc6bc
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-14 11:14:35 -0500

    day39: Recap

--- src/day39_Recap/shapeTask/Square.java
+++ src/day39_Recap/shapeTask/Square.java
@@ -1,6 +1,29 @@
 package day39_Recap.shapeTask;
 
-public class Square {
+public class Square extends Shape{
+
+    private double side;
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        if(side <= 0){
+            System.err.println("Invalid Side: "+side);
+            System.exit(1);
+        }
+
+        this.side = side;
+    }
+
+    //                              -100
+    public Square(double side) {
+        super("Square");
+        setSide(side);
+    }
+
+
 
 
 }
