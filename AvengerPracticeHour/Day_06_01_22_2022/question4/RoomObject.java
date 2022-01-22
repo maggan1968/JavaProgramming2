f4ff3ffe8894125ad2e04e32773a8b19be8392fe
Baros2535 <brsaltn.25@gmail.com>
2022-01-22 19:09:19 +0300

    Question4 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/question4/RoomObject.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question4/RoomObject.java
@@ -4,8 +4,8 @@ public class RoomObject {
     public static void main(String[] args) {
         Room room = new Room();
         room.setInfo(5, 11);
-        System.out.print(room);
-
+        System.out.println(room);
+        System.out.println(room.print());
 
     }
 }
