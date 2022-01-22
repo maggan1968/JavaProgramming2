1cb8f09ae83118d6130f9497a064f485134986e9
Baros2535 <brsaltn.25@gmail.com>
2022-01-22 19:08:54 +0300

    Question3 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/Question3.java
+++ AvengerPracticeHour/Day_06_01_22_2022/Question3.java
@@ -13,10 +13,10 @@ public class Question3 {
 
     public static void main(String[] args) {
         Question3 obj = new Question3();
-        obj.m(5);
-        obj.m();
-        obj.count += 10;
-        obj.m(2);
+        obj.m(5); //count=0*5; -> count =0
+        obj.m();     // count=count+1; -> count=1
+        obj.count += 10; // count=1+10 -> count=11
+        obj.m(2);  //count=11*2 -> count=22
         System.out.print(obj.count);
 
 
