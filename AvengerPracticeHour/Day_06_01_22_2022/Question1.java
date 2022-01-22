da6ef732c9aae3d3e072edfeb0216ea2fc4bc15d
Baros2535 <brsaltn.25@gmail.com>
2022-01-22 19:08:38 +0300

    Question1 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/Question1.java
+++ AvengerPracticeHour/Day_06_01_22_2022/Question1.java
@@ -4,17 +4,24 @@ public class Question1 {
     String name;
     int numOfTests;
 
-    /* line 7*/ {
+   public void setInfo(String name,int numOfTests){
         this.name = name;
         this.numOfTests = numOfTests;
 
     }
 
+    @Override
+    public String toString() {
+        return "{" +
+                "name='" + name + '\'' +
+                ", numOfTests=" + numOfTests +
+                '}';
+    }
 
     public static void main(String[] args) {
         Question1 t = new Question1();
 
-        //  t.setInfo("smoke test", 15);
+          t.setInfo("smoke test", 15);
         //I want to get this output {name='smoke test', numOfTests=15}
         System.out.println("t = " + t);
     }
