b5552cb0e728f0b6165ded968364c53947062d01
Baros2535 <brsaltn.25@gmail.com>
2022-01-22 19:46:10 +0300

    Question8 is solved

--- AvengerPracticeHour/Day_06_01_22_2022/question8/a/A.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question8/a/A.java
@@ -2,10 +2,13 @@ package Day_06_01_22_2022.question8.a;
 
 public class A {
 
-
     public static int num1 = 100;
-    private  static int num2 = 200;
+    private static int num2 = 200;
     protected static int num3 = 300;
     static int num4 = 400;
+
+    public static int getNum2() {
+        return num2;
+    }
 
 }
--- AvengerPracticeHour/Day_06_01_22_2022/question8/b/B.java
+++ AvengerPracticeHour/Day_06_01_22_2022/question8/b/B.java
@@ -6,9 +6,9 @@ import Day_06_01_22_2022.question8.a.A;
 public class B extends A {
 
     public static void main(String[] args) {
-        //  System.out.println(num1); // line 1
-        //   System.out.println(num2); // line 2
-        //    System.out.println(num3); // line 3
+        System.out.println(num1); // line 1
+        System.out.println(getNum2()); // line 2
+        System.out.println(num3); // line 3
         // System.out.println(num4) ; // line 4
 
     }
