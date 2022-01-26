9fa0a8019cfec8a0972f9ed0f5817069cdf6b8f7
Aysun120 <aysundamar114@gmail.com>
2022-01-26 15:05:17 -0600

    Final and Encapsulation codes added

---
+++ officeHour/practice_01_26_2022/AddressObject.java
@@ -0,0 +1,21 @@
+package practice_01_26_2022;
+
+public class AddressObject {
+
+    public static void main(String[] args) {
+
+        Address home = new Address();
+        home.setStreet("1234 Abcd rd. Suite 756");
+        home.setCity("Chicago");
+        home.setState("IL");
+        home.setZipCode("57896");
+
+        System.out.println("Home address: "+ home);
+        home.setZipCode("578762");
+        home.setStreet("");
+        System.out.println("Home address: "+home);
+
+        Address work = new Address("1234 Abcd rd. Suite 756","Chicago","IL","56784");
+        System.out.println("Work Address: "+ work);
+    }
+}
