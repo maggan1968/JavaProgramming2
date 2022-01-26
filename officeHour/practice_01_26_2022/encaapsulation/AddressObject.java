08aca7c40eb0121d78b869a4d53768de27fbdbba
Aysun120 <aysundamar114@gmail.com>
2022-01-26 17:33:08 -0600

    Notes added

--- officeHour/practice_01_26_2022/encaapsulation/AddressObject.java
+++ officeHour/practice_01_26_2022/encaapsulation/AddressObject.java
@@ -11,8 +11,12 @@ public class AddressObject {
         home.setZipCode("57896");
 
         System.out.println("Home address: "+ home);
+
         home.setZipCode("578762");
         home.setStreet("");
+        System.out.println(home.getStreet());
+        home.setZipCode("67895");
+
         System.out.println("Home address: "+home);
 
         Address work = new Address("1234 Abcd rd. Suite 756","Chicago","IL","56784");
