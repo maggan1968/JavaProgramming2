effa60d4c46363abe9ed54db09b3d00ec35b2a61
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-01-31 14:28:59 -0500

    day43: Abstraction: Abstract class & Abstract method

--- src/day43_Abstraction/car/Honda.java
+++ src/day43_Abstraction/car/Honda.java
@@ -6,9 +6,6 @@ public class Honda extends Car  {
         super(brand, model, color, year, price);
     }
 
-    public void stop() {
-        System.out.println("");
-    }
 
     public void start(){
         System.out.println("Twist the key to ignition");
