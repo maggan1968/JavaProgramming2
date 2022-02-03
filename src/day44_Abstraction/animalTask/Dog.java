ce77de82572e2c2a241fa31355bbdc3c7847a214
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-03 12:18:05 -0500

    day44: Abstraction - Interface

--- src/day44_Abstraction/animalTask/Dog.java
+++ src/day44_Abstraction/animalTask/Dog.java
@@ -1,6 +1,6 @@
 package day44_Abstraction.animalTask;
 
-public class Dog extends Animal{
+public final class Dog extends Animal{
 
     public Dog(String name, String breed, char gender, int age, String size, String color) {
         super(name, breed, gender, age, size, color);
@@ -10,7 +10,7 @@ public class Dog extends Animal{
         System.out.println(getName()+" is barking");
     }
 
-    @Override
+
     public void eat() {
         System.out.println(getName()+" eats Pizza");
     }
