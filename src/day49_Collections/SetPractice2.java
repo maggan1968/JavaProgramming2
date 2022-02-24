c837ec54ba2bad2d150f5a3a990368cff71715d7
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-24 11:30:18 -0500

    day49: Collections Continue

---
+++ src/day49_Collections/SetPractice2.java
@@ -0,0 +1,13 @@
+package day49_Collections;
+
+public class SetPractice2 {
+
+    public static void main(String[] args) {
+
+        String[] arr = {"Wooden Spoon", "Book", "Pen", "Phone", "Wooden Spoon",
+                "Wooden Spoon", "Wooden Spoon", "Wooden Spoon", "Milk", "Eggs", "Coke", "Paper towels"};
+
+
+    }
+
+}
