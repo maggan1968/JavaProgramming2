33d8330a05f637614dcc7a36f808d1ae0929c856
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-24 12:31:10 -0500

    day49: Collections Continue

--- src/day49_Collections/SetPractice2.java
+++ src/day49_Collections/SetPractice2.java
@@ -45,11 +45,33 @@ public class SetPractice2 {
                         Do not change the order of the elements
          */
 
+
+        Integer[] nums = {1,2,1,1,2,2,2,3,3,3,3,2,3,4,5,1,2,3,4,5,1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10};
+
+        nums = new LinkedHashSet<>(  Arrays.asList(nums) ).toArray( new Integer[0] );
+
+        System.out.println(Arrays.toString(nums) );
+
+
         System.out.println("-----------------------------------------------");
 
-        String str = "aaaabbbbccccdddeeeee";
+        String str = "eeeeeaaaabbbbccccdddeeeee";
 
-        String result = ""; //a4b4c4d3e5
+                    //  eabcd
+                    //  54434
+
+
+        String result = ""; //e5a4b4c4d3
+
+
+        for (String each : new LinkedHashSet<>(Arrays.asList(str.split("")))) {
+             result += each + Collections.frequency(  Arrays.asList(str.split("")) ,  each);
+        }
+
+
+        System.out.println(result);
+
+
 
 
 
