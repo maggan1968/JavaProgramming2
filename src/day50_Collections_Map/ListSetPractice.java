5ad955c10b9f80ebfc750563452b39a2398f5910
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-28 10:59:19 -0500

    day50: Collections Continue - Iterable

--- src/day50_Collections_Map/ListSetPractice.java
+++ src/day50_Collections_Map/ListSetPractice.java
@@ -19,9 +19,40 @@ public class ListSetPractice {
         System.out.println(list);
 
 
-        List<String> names = null;
-
+      //  List<String> names = null;
        // System.out.println(names.size());
+
+        System.out.println("----------------------------------------");
+
+        // pop() ==> LIFO
+        List<Character> chars = new Stack<>();
+        chars.addAll(Arrays.asList('A', 'B', 'C', 'D'));
+
+        System.out.println("chars = " + chars); //[A, B, C, D]
+
+        (  (Stack)chars ).pop();
+
+        System.out.println("chars = " + chars); //[A, B, C]
+
+
+        //poll() ==> FIFO
+
+        List<String> names = new LinkedList<>();
+        names.addAll(Arrays.asList("James", "Jimmy", "Kathy", "Breanna", "Max"));
+
+        System.out.println(names);
+
+        ((LinkedList<String>) names).poll();
+
+        System.out.println(names);
+
+        ((LinkedList<String>) names).poll();
+
+        System.out.println(names);
+
+
+
+
 
 
 
