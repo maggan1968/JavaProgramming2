0ad7987eab43f4efb0af76a08032d3c6cc90c1f0
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-28 12:34:43 -0500

    day50: Collections Continue - Iterable

---
+++ src/day50_Collections_Map/RemovePalindromes.java
@@ -0,0 +1,20 @@
+package day50_Collections_Map;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RemovePalindromes {
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+        names.addAll(
+                Arrays.asList("Java", "Python", "Cydeo", "Car", "Level", "civic", "radar", "kayak", "reviver", "racecar", "madam")
+        );
+
+
+    }
+
+
+}
