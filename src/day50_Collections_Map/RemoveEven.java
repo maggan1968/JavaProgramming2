be4198306a8bc104e7b62779b9bbf66d4918cb8a
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-02-28 12:56:45 -0500

    day50: Collections Continue - Iterable

--- src/day50_Collections_Map/RemoveEven.java
+++ src/day50_Collections_Map/RemoveEven.java
@@ -1,4 +1,15 @@
 package day50_Collections_Map;
 
+import java.util.Arrays;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
 public class RemoveEven {
+
+    public static void main(String[] args) {
+
+        Set<Integer> set = new LinkedHashSet<>(Arrays.asList(1,2,3,4,5,6,100, 90, 80, 75, 45, 35, 25, 15));
+
+    }
+
 }
