d040e94c48bf2817a8ce0ce087127d11142a1ce2
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-01 15:46:45 -0500

    day51: Maps Intro

---
+++ src/day51_Map/MapPractice4.java
@@ -0,0 +1,16 @@
+package day51_Map;
+
+public class MapPractice4 {
+
+    public static void main(String[] args) {
+
+        String[] group1 = {"Duygu","Magda","Farangez","Edison","Kerem","Farangis"};
+        String[]group2 = {"Margie","Vesele","Iryn","Nilufar","Aziza","Nurcan","Raika","Igor","Elif","Sana","Azamat"};
+        String [] group9 = {"Alex","Ali","Andriy","Cassandra","Sevgul","Olha","Tito","Ozan","Serkan","Furkan","Madina","Muharrem"};
+        String[] group24= {"Maria","Kristina","Amin","George","Layan","Amin","Yulia","George","Ayanle","Maxim","Nermin","Doughlas"};
+        String[] group5 = {"Nazar","Veronika","Mikael","Layla","Badmaa","Zaeir","Anna","Tamara","Eugene"};
+
+
+    }
+
+}
