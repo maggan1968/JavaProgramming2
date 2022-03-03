24d51332e195e64748aca10964facbbe4fc74797
MuhtarMahmut <Muhtar.Mahmut0519@gmail.com>
2022-03-03 12:26:36 -0500

    day52: Map Continue

---
+++ src/day52_Map_FunctionalInterface/Test.java
@@ -0,0 +1,17 @@
+package day52_Map_FunctionalInterface;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        // function1: create a function that can display a number is odd
+
+
+        // function2: create a function that can check if a person is eligible to buy alcohol
+
+
+        //function3: create a function that can display the cube of a number
+
+    }
+
+}
