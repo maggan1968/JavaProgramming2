29ccc3f5d2f214af30fcfcadb3bdf6b27770fdb6
Malgorzata Altunsayar <mkowalczyk42@gmail.com>
2022-05-29 01:24:40 -0500

    initial commit

--- src/day29_ArrayList/ReverseArrayAndArrayList.java
+++ src/day29_ArrayList/ReverseArrayAndArrayList.java
@@ -7,6 +7,14 @@ public class ReverseArrayAndArrayList {
 
     public static void main(String[] args) {
 
+        /**
+         * you create an empty array (here result)with the same length of your array you wanna reverse
+         * you use fori loop from the last index to 0
+         * you create an additional variable j for index numbers for the empty array result, i is the old array indexes
+         * you assign old array element to new result array
+         * you print it outside of the loop
+         */
+
         int[] array = {1,2,3,4,5,6};
 
         int[] result = new int[array.length];  // {6,5,4,3,2,1}
@@ -18,7 +26,7 @@ public class ReverseArrayAndArrayList {
 
         System.out.println(Arrays.toString(result));
 
-        System.out.println("---------------------------------------");
+        System.out.println("----------interview way for array list reverse-----------------------------");
 
         ArrayList<Integer> list = new ArrayList<>();
         list.addAll(Arrays.asList(1,2,3,4,5,6,7,8));
@@ -27,8 +35,9 @@ public class ReverseArrayAndArrayList {
 
         for (int i = list.size() - 1; i >= 0; i--) {
 
-            int each = list.get(i);
-            reversedList.add(each);
+            int each = list.get(i);// we need index thats why we create int each here and we assign
+                                   //element one by one from last element to 0
+            reversedList.add(each);//we add each to the reversed list
 
         }
 
